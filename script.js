c91e107db1aac76ceb420fdcbe5b5e153eaf99fd
weerawardanakrishantha <weerawardanakrishantha@gmail.com>
2025-03-23 09:21:32 +0530

    first

---
+++ script.js
@@ -0,0 +1,21 @@
+
+let systemNuber=10;
+
+let text=document.getElementById("text");
+
+function m(){  
+        let gessValue=document.getElementById("number").value;
+        let gess=parseInt(gessValue);
+        document.getElementById("number").value=""; 
+        
+            if(gess>systemNuber){            
+                text.innerText="too high.. gess again";
+            }else if(gess<systemNuber){        
+                text.innerText="too low.. gess again";
+            }else{
+            text.innerText="congratulation..you are a winner";
+            }              
+    }        
+
+    
+    
